7c3d4c1908d6364a285b50bafeb749fe08eb3f94
VadimSquorpikkor <vadimserikov11@gmail.com>
2017-03-30 13:54:49 +0300

    4

--- app/src/main/java/com/squorpikkor/android/app/sborka/MainActivity.java
+++ app/src/main/java/com/squorpikkor/android/app/sborka/MainActivity.java
@@ -7,28 +7,26 @@ import android.support.v7.widget.SearchView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Device[] deviceListArray = new Device[]{
-            new Device("6102"),
-            new Device("6130")
-    };
-
-
-    ArrayList<Device> deviceList = new ArrayList<>();
+    ArrayList<Device> deviceList;
     Button addButton;
-    Device device;
-    Detail detail;
+    /*Device device;
+    Detail detail;*/
 
-    TextView text1, text2;
+    TextView text1, text2, newDevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        deviceList = new ArrayList<>();
+        deviceList.add(new Device(getString(R.string.device01)));
 
         int[] nameIdArray = {
                 R.string.name01,
@@ -43,22 +41,24 @@ public class MainActivity extends AppCompatActivity {
         addButton = (Button) findViewById(R.id.addButton);
         text1 = (TextView) findViewById(R.id.text1);
         text2 = (TextView) findViewById(R.id.text2);
+        newDevice = (TextView) findViewById(R.id.newDeviceInput);
+
 
         for (int i = 0; i < nameIdArray.length; i++) {
-            deviceListArray[0].addDetail(getString(nameIdArray[i]), getString(numIdArray[i]));
+            deviceList.get(i).addDetail(getString(nameIdArray[i]), getString(numIdArray[i]));
         }
 
 
 
-        text1.setText(deviceListArray[0].getDetail(0).getName());
-        text2.setText(deviceListArray[0].getDetail(0).getNumber());
+        text1.setText(deviceList.get(0).getDetail(0).getName());
+        text2.setText(deviceList.get(0).getDetail(0).getNumber());
 
         View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch (v.getId()) {
                     case R.id.addButton:
-                        new Device("NAME");
+                        addDevice();
                         break;
                 }
 
@@ -66,5 +66,23 @@ public class MainActivity extends AppCompatActivity {
         };
 
         addButton.setOnClickListener(clickListener);
+    }
+
+    void addDevice() {
+        String name = newDevice.getText().toString();
+        if (name.length() == 0) {
+            Toast.makeText(this, R.string.add_device_error, Toast.LENGTH_LONG).show();
+        } else {
+            deviceList.add(new Device(name));
+        }
+
+    }
+
+    void saveAll() {
+
+    }
+
+    void loadAll() {
+
     }
 }
